7a8e649a01c871e30479a6143529bc356ab3f798
Boni García <boni.garcia@uc3m.es>
2022-02-13 17:38:34 +0100

    Include linear layout app example

---
+++ LinearLayout/app/src/main/java/io/github/bonigarcia/android/linearlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.bonigarcia.android.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
