1a9c3da512d0b6114ceffedbd660e726ffea9010
Boni García <boni.garcia@uc3m.es>
2022-02-24 11:35:32 +0100

    Include Broadcast Receiver demo app

---
+++ BroadcastReceiver/app/src/main/java/io/github/bonigarcia/android/broadcastreceiver/MainActivity.java
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2022 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.android.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("io.github.bonigarcia.sendbroadcast");
+                sendBroadcast(intent);
+            }
+        });
+
+        configureReceiver();
+    }
+
+    private void configureReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("io.github.bonigarcia.sendbroadcast");
+        filter.addAction("android.intent.action.ACTION_POWER_CONNECTED");
+        filter.addAction("android.intent.action.ACTION_POWER_DISCONNECTED");
+        receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
+    }
+
+}
