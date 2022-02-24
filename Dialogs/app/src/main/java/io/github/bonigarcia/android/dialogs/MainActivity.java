0ca7c26d475c19ac1af4a6b97872b7097de7bc62
Boni García <boni.garcia@uc3m.es>
2022-02-24 12:28:49 +0100

    Include sample apps using dialogs and toasts

---
+++ Dialogs/app/src/main/java/io/github/bonigarcia/android/dialogs/MainActivity.java
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2022 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.android.dialogs;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendName(View view) {
+        EditText nameText = findViewById(R.id.editText);
+
+        String name = nameText.getText().toString();
+        if (name.isEmpty()) {
+            AlertDialog.Builder ad = new AlertDialog.Builder(this);
+            ad.setTitle(R.string.title_dialog);
+            ad.setMessage(R.string.message_dialog);
+            ad.setPositiveButton(R.string.button_ok,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int arg1) {
+                            dialog.cancel();
+                        }
+                    });
+            ad.show();
+        } else {
+            Intent intent = new Intent(this, SecondActivity.class);
+            Bundle b = new Bundle();
+            b.putString("name", name);
+            intent.putExtras(b);
+            startActivity(intent);
+        }
+    }
+
+}
