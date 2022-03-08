23fae7aeb89e6e97e8a5c36cf05353092897255f
Boni García <boni.garcia@uc3m.es>
2022-03-08 17:09:41 +0100

    Include basic demo implementing a REST client using JSON

---
+++ RestClientJSON/app/src/main/java/io/github/bonigarcia/android/restclientjson/MainActivity.java
@@ -0,0 +1,76 @@
+/*
+ * (C) Copyright 2022 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.android.restclientjson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getPosts();
+    }
+
+    private void getPosts() {
+        List<String> postTitles = new ArrayList<>();
+        ArrayAdapter listAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, postTitles);
+        ListView list = findViewById(R.id.list);
+        list.setAdapter(listAdapter);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        PostService postService = retrofit.create(PostService.class);
+        Call<List<Post>> call = postService.getPost();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                for (Post post : response.body()) {
+                    postTitles.add(post.getTitle());
+                }
+                listAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.e(this.getClass().getSimpleName(), "Exception calling " +
+                        "endpoint", t);
+            }
+        });
+    }
+}
