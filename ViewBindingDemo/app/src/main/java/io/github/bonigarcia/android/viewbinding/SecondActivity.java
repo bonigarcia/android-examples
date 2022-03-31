957e2c284b83e5d19de8c1de41d5d8b232abcee0
Boni Garcia <boni.garcia@uc3m.es>
2022-03-31 10:20:58 +0200

    Include view bindings demo

---
+++ ViewBindingDemo/app/src/main/java/io/github/bonigarcia/android/viewbinding/SecondActivity.java
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright 2022 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.android.viewbinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Bundle bundle = getIntent().getExtras();
+        Resources resources = getResources();
+        String text = String.format(resources.getString(R.string.hello),
+                bundle.getString("name"));
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(text);
+    }
+}
