a58288813981cfb8ab8c075c3ec93d9b77c1bc41
Boni Garcia <boni.garcia@uc3m.es>
2023-02-13 15:51:11 +0100

    Include button to remove elements in adapter demo

--- Adapter/app/src/main/java/es/uc3m/android/adapter/MainActivity.java
+++ Adapter/app/src/main/java/es/uc3m/android/adapter/MainActivity.java
@@ -53,7 +53,13 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.action_add) {
             long currentTimeMillis = System.currentTimeMillis();
             String formatDate = formatDate(currentTimeMillis);
-            listAdapter.add(formatDate);
+            listAdapter.add(listAdapter.getCount() + " " + formatDate);
+            return true;
+        } else if (item.getItemId() == R.id.action_remove) {
+            int count = listAdapter.getCount();
+            if (count > 0) {
+                listAdapter.remove(listAdapter.getItem(0));
+            }
             return true;
         } else {
             return super.onOptionsItemSelected(item);
