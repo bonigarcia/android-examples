a6e9bf887fcde006848b6b423744e98d5228ace3
Boni Garcia <boni.garcia@uc3m.es>
2023-02-13 12:11:25 +0100

    Include adapter demo

---
+++ Adapter/app/src/main/java/es/uc3m/android/adapter/MainActivity.java
@@ -0,0 +1,69 @@
+/*
+ * (C) Copyright 2022 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package es.uc3m.android.adapter;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> listAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.list_view);
+        listAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1);
+        listView.setAdapter(listAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_add) {
+            long currentTimeMillis = System.currentTimeMillis();
+            String formatDate = formatDate(currentTimeMillis);
+            listAdapter.add(formatDate);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public String formatDate(long timeMillis) {
+        DateFormat formatter = DateFormat.getDateTimeInstance();
+        Date date = new Date(timeMillis);
+        return formatter.format(date);
+    }
+
+}
