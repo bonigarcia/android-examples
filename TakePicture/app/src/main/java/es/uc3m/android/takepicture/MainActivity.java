a81f1874fe8b8a550d7429094b59307cb8a78e53
Boni Garcia <boni.garcia@uc3m.es>
2023-02-23 13:58:50 +0100

    Update take picture demo

--- TakePicture/app/src/main/java/es/uc3m/android/takepicture/MainActivity.java
+++ TakePicture/app/src/main/java/es/uc3m/android/takepicture/MainActivity.java
@@ -16,13 +16,6 @@
  */
 package es.uc3m.android.takepicture;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -30,13 +23,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,49 +46,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            // Ensure that there's a camera activity to handle the intent
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                File photoFile = createImageFile();
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "es.uc3m.android.takepicture.provider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    activityResultLauncher.launch(takePictureIntent);
+                }
+            }
+        });
+    }
+
+    private File createImageFile() {
+        File image = null;
+        try {
+            DateFormat formatter = DateFormat.getDateTimeInstance();
+            String currentDate = formatter.format(new Date());
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            image = File.createTempFile(currentDate, ".jpg", storageDir);
+            currentPhotoPath = image.getAbsolutePath();
+        } catch (IOException e) {
+            Log.e(this.getLocalClassName(), "Exception creating file for image", e);
+        }
+        return image;
     }
 
     ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK) {
-                        Bitmap miBitmap = BitmapFactory.decodeFile(currentPhotoPath);
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Bitmap miBitmap = BitmapFactory.decodeFile(currentPhotoPath);
 
-                        ImageView imageView = findViewById(R.id.image);
-                        imageView.setImageBitmap(miBitmap);
+                    ImageView imageView = findViewById(R.id.image);
+                    imageView.setImageBitmap(miBitmap);
 
-                        TextView textView = findViewById(R.id.text);
-                        textView.setText(currentPhotoPath);
-                    }
+                    TextView textView = findViewById(R.id.text);
+                    textView.setText(currentPhotoPath);
                 }
             });
-
-    public void onClickButton(View v) throws IOException {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = createImageFile();
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "es.uc3m.android.takepicture.provider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                activityResultLauncher.launch(takePictureIntent);
-            }
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileNamePrefix = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileNamePrefix, ".jpg", storageDir);
-
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
 
 }
