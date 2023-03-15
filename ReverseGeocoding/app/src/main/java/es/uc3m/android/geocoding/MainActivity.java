e8dc7eb45f4565dcef031c2e673ac87a0f835910
Boni Garcia <boni.garcia@uc3m.es>
2023-03-15 17:24:54 +0100

    Simplify reverse geocoding demo

--- ReverseGeocoding/app/src/main/java/es/uc3m/android/geocoding/MainActivity.java
+++ ReverseGeocoding/app/src/main/java/es/uc3m/android/geocoding/MainActivity.java
@@ -34,6 +34,9 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
+    private static final int REQUEST_PERMISSION_ACCESS_FINE_LOCATION = 1;
+    private static final String INTRO = "\r\n";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,33 +44,47 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSION_ACCESS_FINE_LOCATION);
+        } else {
+            LocationManager locationManager =
+                    (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, this);
         }
-
-        LocationManager locationManager =
-                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, this);
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        if (Geocoder.isPresent()) {
-            Geocoder gc = new Geocoder(this, Locale.getDefault());
-            try {
-                List<Address> addresses =
-                        gc.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                String addressesStr = "";
-                for (Address address : addresses) {
-                    addressesStr += address.getAddressLine(0) + "\r\n";
-                }
-                TextView textView = findViewById(R.id.msg);
-                textView.setText(addressesStr);
-
-            } catch (Exception e) {
-                Log.e(this.getLocalClassName(), "Exception getting location", e);
+        Geocoder gc = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses =
+                    gc.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            String addressesStr = "";
+            for (Address address : addresses) {
+                addressesStr += address.getAddressLine(0) + INTRO;
             }
-        } else {
-            Log.w(this.getLocalClassName(), "Geocoder not available");
+            TextView textView = findViewById(R.id.msg);
+            textView.setText(addressesStr);
+
+        } catch (Exception e) {
+            Log.e(this.getLocalClassName(), "Exception invoking geocoder", e);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_PERMISSION_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    finish();
+                    startActivity(getIntent());
+                }
+                break;
+            }
         }
     }
 
