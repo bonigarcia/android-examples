66f04c1e80c4929581d948de9eb356478d41a733
Boni Garcia <boni.garcia@uc3m.es>
2024-03-04 15:10:08 +0100

    Clean activity in two activities with result demo

--- TwoActivitiesWithResult/app/src/main/java/es/uc3m/android/twoactivities/SecondActivity.java
+++ TwoActivitiesWithResult/app/src/main/java/es/uc3m/android/twoactivities/SecondActivity.java
@@ -17,7 +17,6 @@
 package es.uc3m.android.twoactivities;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
