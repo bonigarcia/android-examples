a2fe8b977b26c5313575ef3b6cc09b1224444546
Boni Garcia <boni.garcia@uc3m.es>
2025-02-05 15:09:47 +0100

    Include navigation drawer in scaffold demo

--- Scaffold/app/src/main/java/es/uc3m/android/scaffold/MainActivity.kt
+++ Scaffold/app/src/main/java/es/uc3m/android/scaffold/MainActivity.kt
@@ -4,31 +4,54 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import es.uc3m.android.scaffold.ui.theme.MyAppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -45,34 +68,108 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyScreen() {
-    Scaffold(
-        topBar = {
-            MyTopAppBar()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            MyDrawerContent(scope, drawerState)
         },
-        floatingActionButton = {
-            MyFloatingActionButtons()
-        },
-        content = { innerPadding ->
-            MyContent(Modifier.padding(innerPadding))
-        },
-        bottomBar = {
-            MyNavigationBar()
-        }
+    ) {
+        Scaffold(
+            topBar = {
+                MyTopAppBar(scope, drawerState)
+            },
+            floatingActionButton = {
+                MyFloatingActionButtons()
+            },
+            content = { innerPadding ->
+                MyContent(Modifier.padding(innerPadding))
+            },
+            bottomBar = {
+                MyNavigationBar()
+            }
+        )
+    }
+}
+
+data class NavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
+
+@Composable
+fun MyDrawerContent(scope: CoroutineScope, drawerState: DrawerState) {
+    var selectedItem by remember { mutableIntStateOf(0) }
+
+    val items = listOf(
+        NavigationItem(
+            title = stringResource(R.string.home),
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        NavigationItem(
+            title = stringResource(R.string.profile),
+            selectedIcon = Icons.Filled.Person,
+            unselectedIcon = Icons.Outlined.Person
+        ),
+        NavigationItem(
+            title = stringResource(R.string.settings),
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings
+        )
     )
+    ModalDrawerSheet {
+        Spacer(modifier = Modifier.height(24.dp))
+        items.forEachIndexed { index, item ->
+            NavigationDrawerItem(
+                label = {
+                    Text(text = item.title)
+                },
+                selected = index == selectedItem,
+                onClick = {
+                    selectedItem = index
+                    scope.launch {
+                        drawerState.close()
+                    }
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (index == selectedItem) item.selectedIcon else item.unselectedIcon,
+                        contentDescription = item.title
+                    )
+                },
+                modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+            )
+        }
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(scope: CoroutineScope, drawerState: DrawerState) {
     TopAppBar(
         navigationIcon = {
-            IconButton(onClick = { /* TODO */ }) {
-                Icon(imageVector = Icons.Rounded.Menu, contentDescription = null)
+            IconButton(onClick = {
+                scope.launch {
+                    drawerState.apply {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Rounded.Menu,
+                    stringResource(R.string.menu)
+                )
             }
         },
         title = { Text(text = stringResource(R.string.app_name)) },
         actions = {
-//            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
             IconButton(onClick = { /* TODO */ }) {
                 Icon(
                     imageVector = Icons.Rounded.Search,
@@ -85,7 +182,6 @@ fun MyTopAppBar() {
                     contentDescription = stringResource(R.string.account)
                 )
             }
-//            }
         })
 }
 
@@ -100,7 +196,7 @@ fun MyFloatingActionButtons() {
 fun MyContent(modifier: Modifier = Modifier) {
     Text(
         text = stringResource(R.string.content_text),
-        modifier = modifier
+        modifier = modifier.padding(16.dp)
     )
 }
 
