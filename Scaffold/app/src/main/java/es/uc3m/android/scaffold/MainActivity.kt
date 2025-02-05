a261c04feaeca94d7b0b20942a5304995366fcf4
Boni Garcia <boni.garcia@uc3m.es>
2025-02-05 15:29:28 +0100

    Improve bottom navigation bar in scaffold app

--- Scaffold/app/src/main/java/es/uc3m/android/scaffold/MainActivity.kt
+++ Scaffold/app/src/main/java/es/uc3m/android/scaffold/MainActivity.kt
@@ -9,11 +9,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Home
@@ -30,7 +27,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
@@ -66,34 +62,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun MyScreen() {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            MyDrawerContent(scope, drawerState)
-        },
-    ) {
-        Scaffold(
-            topBar = {
-                MyTopAppBar(scope, drawerState)
-            },
-            floatingActionButton = {
-                MyFloatingActionButtons()
-            },
-            content = { innerPadding ->
-                MyContent(Modifier.padding(innerPadding))
-            },
-            bottomBar = {
-                MyNavigationBar()
-            }
-        )
-    }
-}
-
 data class NavigationItem(
     val title: String,
     val selectedIcon: ImageVector,
@@ -101,9 +69,8 @@ data class NavigationItem(
 )
 
 @Composable
-fun MyDrawerContent(scope: CoroutineScope, drawerState: DrawerState) {
-    var selectedItem by remember { mutableIntStateOf(0) }
-
+fun MyScreen() {
+    // Items for the navigation drawer and bottom bar
     val items = listOf(
         NavigationItem(
             title = stringResource(R.string.home),
@@ -121,6 +88,37 @@ fun MyDrawerContent(scope: CoroutineScope, drawerState: DrawerState) {
             unselectedIcon = Icons.Outlined.Settings
         )
     )
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            MyDrawerContent(items, scope, drawerState)
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                MyTopAppBar(scope, drawerState)
+            },
+            floatingActionButton = {
+                MyFloatingActionButtons()
+            },
+            content = { innerPadding ->
+                MyContent(Modifier.padding(innerPadding))
+            },
+            bottomBar = {
+                MyNavigationBar(items)
+            }
+        )
+    }
+}
+
+@Composable
+fun MyDrawerContent(items: List<NavigationItem>, scope: CoroutineScope, drawerState: DrawerState) {
+    var selectedItem by remember { mutableIntStateOf(0) }
+
+
     ModalDrawerSheet {
         Spacer(modifier = Modifier.height(24.dp))
         items.forEachIndexed { index, item ->
@@ -145,7 +143,6 @@ fun MyDrawerContent(scope: CoroutineScope, drawerState: DrawerState) {
             )
         }
     }
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -202,35 +199,29 @@ fun MyContent(modifier: Modifier = Modifier) {
 
 
 @Composable
-fun MyNavigationBar() {
-    NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
-        NavigationBarItem(
-            selected = true,
-            onClick = { /* TODO */ },
-            icon = { Icon(Icons.Filled.Home, stringResource(R.string.home)) },
-            label = { Text(text = stringResource(R.string.home)) }
-        )
-        NavigationBarItem(
-            selected = false,
-            onClick = { /* TODO */ },
-            icon = { Icon(Icons.Filled.Favorite, stringResource(R.string.favorite)) },
-            label = { Text(text = stringResource(R.string.favorite)) }
-        )
-        NavigationBarItem(
-            selected = false,
-            onClick = { /* TODO */ },
-            icon = { Icon(Icons.Filled.Info, stringResource(R.string.info)) },
-            label = { Text(text = stringResource(R.string.info)) }
-        )
-        NavigationBarItem(
-            selected = false,
-            onClick = { /* TODO */ },
-            icon = { Icon(Icons.Filled.Place, stringResource(R.string.places)) },
-            label = { Text(text = stringResource(R.string.places)) }
-        )
+fun MyNavigationBar(items: List<NavigationItem>) {
+    var selectedItem by remember { mutableIntStateOf(0) }
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = index == selectedItem,
+                onClick = {
+                    selectedItem = index
+                },
+                label = {
+                    Text(text = item.title)
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (index == selectedItem) item.selectedIcon else item.unselectedIcon,
+                        contentDescription = item.title
+                    )
+                }
+            )
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
