9e62dd6414ec7d159b041a6e50e54f1f57b019cd
Boni Garcia <boni.garcia@uc3m.es>
2025-02-06 10:45:32 +0100

    Rename navigation with arguments demo app

---
+++ NavigationArguments/app/src/androidTest/java/es/uc3m/android/navigationargs/AndroidTest.kt
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2025 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package es.uc3m.android.navigationargs
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](https://developer.android.com/studio/test)
+ */
+@RunWith(AndroidJUnit4::class)
+class AndroidTest {
+    @Test
+    fun test() {
+        // TODO: Exercise
+
+        // TODO: Verify
+    }
+}
