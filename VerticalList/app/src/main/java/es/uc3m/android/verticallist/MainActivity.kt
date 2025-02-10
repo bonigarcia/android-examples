113ec1de739c4989987252977a4a7574311ff046
Boni Garcia <boni.garcia@uc3m.es>
2025-02-10 11:52:19 +0100

    Rename list and grids demo apps

--- VerticalList/app/src/main/java/es/uc3m/android/verticallist/MainActivity.kt
+++ VerticalList/app/src/main/java/es/uc3m/android/verticallist/MainActivity.kt
@@ -20,25 +20,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import es.uc3m.android.verticallist.ui.theme.MyAppTheme
@@ -65,6 +61,12 @@ data class Item(val title: String, val description: String)
 fun MyLazyColumn(modifier: Modifier = Modifier) {
     val title = stringResource(R.string.item_title)
     val description = stringResource(R.string.item_description)
+    val myList = (0..20).map {
+        Item(
+            title = String.format(title, it + 1),
+            description = String.format(description, it + 1),
+        )
+    }
 
     LazyColumn(
         flingBehavior = ScrollableDefaults.flingBehavior(),
@@ -74,42 +76,23 @@ fun MyLazyColumn(modifier: Modifier = Modifier) {
         content = {
             item {
                 Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
                     text = stringResource(R.string.my_list),
-                    style = MaterialTheme.typography.headlineSmall,
-                    textAlign = TextAlign.Start
-                )
-            }
-            val myList = (0..20).map {
-                Item(
-                    title = String.format(title, it + 1),
-                    description = String.format(description, it + 1),
+                    style = MaterialTheme.typography.headlineSmall
                 )
             }
             items(myList) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .border(
-                            width = 1.dp,
-                            color = Color.LightGray,
-                        ),
+                Column(
+                    modifier = Modifier.padding(8.dp)
                 ) {
-                    Column(
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        Text(
-                            text = it.title,
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                        Text(
-                            text = it.description,
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    }
+                    Text(
+                        text = it.title,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = it.description,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    HorizontalDivider()
                 }
             }
         }
