7e69ccba3ce9291092659b0b739c8d77f5dbbb41
Boni Garcia <boni.garcia@uc3m.es>
2025-04-11 13:18:44 +0200

    Clean and update dependencies in datastore demo app

--- DataStore/app/build.gradle.kts
+++ DataStore/app/build.gradle.kts
@@ -48,8 +48,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.constraintlayout.compose)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.datastore)
