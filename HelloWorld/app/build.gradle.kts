04fa29be3d5ac596704e65b7700ebd9f9c809e5f
Boni Garcia <boni.garcia@uc3m.es>
2026-01-21 14:16:14 +0100

    Bump to target Android SDK 36, Gragle 9.3.0, and Android Gradle plugin 9.0.0

--- HelloWorld/app/build.gradle.kts
+++ HelloWorld/app/build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "es.uc3m.android.helloworld"
-    compileSdk = 35
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
         applicationId = "es.uc3m.android.helloworld"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -30,9 +31,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
