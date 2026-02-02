e5bf55d5ee03d6ac2ed59dc8f71ff7c07c6c1f86
Boni Garcia <boni.garcia@uc3m.es>
2026-02-02 14:32:49 +0100

    Include androidx.compose.material:material-icons-core to use icons

--- ScaffoldNavigation/app/build.gradle.kts
+++ ScaffoldNavigation/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material.icons.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
